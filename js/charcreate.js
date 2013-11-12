af6e590799a71c4294ebeec3ff9003356f0bc88c
marakovitsm <matt@marakovits.net>
2013-11-12 13:34:25 -0500

    Made character creation screen

--- js/charcreate.js
+++ js/charcreate.js
@@ -1,1 +1,12 @@
 //script
+
+function charCreate(name) {
+	if(document.getElementById('genderMale').checked) {
+  		gender="male"
+	}
+	else if(document.getElementById('genderFemale').checked) {
+  		gender="female"
+	}
+alert(name);
+alert(gender);
+}
